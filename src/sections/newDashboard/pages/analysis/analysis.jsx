248b2575b78494d1201a64c51187a1559249e967
Aman Kumar <trulyaman25@gmail.com>
2024-11-08 23:47:54 +0530

    Deployment Issue Fixed Part - 7

--- src/sections/newDashboard/pages/analysis/analysis.jsx
+++ src/sections/newDashboard/pages/analysis/analysis.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import WarningIcon from '../../../../assets/icons/WarningIcon.png';
 import VerifiedDocumentIcon from '../../../../assets/icons/VerifiedDocument.png';
 
 
